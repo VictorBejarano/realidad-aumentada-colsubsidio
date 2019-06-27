5d7bc6aeb428236e82fe728ac7a896729444e52c
VictorBejarano <alejandrobjrn4@gmail.com>
2019-06-27 12:21:36 -0500

    Adicion de la solicitud de permiso

--- app/src/main/java/com/colsubsidio/arprueba/SplashActivity.java
+++ app/src/main/java/com/colsubsidio/arprueba/SplashActivity.java
@@ -1,8 +1,17 @@
 package com.colsubsidio.arprueba;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.colsubsidio.arprueba.augmentedimage.AugmentedImageActivity;
 
@@ -12,7 +21,55 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent intent = new Intent(this, AugmentedImageActivity.class);
-        this.startActivity(intent);
+//
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        // La App esta en ejecución
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Log.e("SplashActvity", "Sasha Permiso concedido");
+
+        } else if (permissionCheck == PackageManager.PERMISSION_DENIED){
+
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                mostrarExplicacion();
+
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 100);
+                Log.e("SplashActvity", "Sasha B acepto");
+            }
+
+        }
+    }
+
+    private void mostrarExplicacion(){
+        new AlertDialog.Builder(this)
+                .setTitle("Autorización")
+                .setMessage("Necesito permiso para acceder a los contactos de tu dispositivo.")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                        }
+
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Mensaje acción cancelada
+                        mensajeAccionCancelada();
+                    }
+                })
+        .show();
+        Log.e("SplashActvity", "Sasha Permiso concedido");
+
+    }
+    public void mensajeAccionCancelada(){
+        Toast.makeText(getApplicationContext(),
+                "Haz rechazado la petición, no puede continuar con la aplicación.",
+                Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
